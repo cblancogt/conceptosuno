63832fc21fb8bf7167e980e87793a45f12ba34da
84MDCZ1\Carlos Roberto <cblancoit@gmail.com>
2022-01-27 11:42:31 -0600

    conceptos basicos iniciando

---
+++ conceptosuno/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace conceptosuno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //usando clases 
+            c_String cadenas = new c_String();
+
+            cadenas.FuncionString();
+
+        }
+
+       
+    }
+}
---
+++ conceptosuno/c_String.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace conceptosuno
+{
+    class c_String
+    {
+        //constructor
+        public c_String() { }
+
+         public void FuncionString()
+        {
+            string palabra = "Carlos Roberto Blanco Osoy";
+
+            Console.WriteLine("Tamaño de la cadena: " + palabra.Length + " Caracteres");
+            Console.WriteLine("SubString es: " + palabra.Substring(7, 5));
+            Console.WriteLine("en mayusculas: " + palabra.ToUpper());
+
+            int numero = 50;
+            string num = numero.ToString();
+            Console.WriteLine("Convertir numero a String " + num);
+        }
+
+
+    }
+}
